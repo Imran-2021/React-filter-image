63f2bc748efcc81ef620d1f119f9eba7a2c776d3
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-11 09:59:19 +0600

    first commit

---
+++ src/Components/ReactGallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FakeData from '../FakeData';
+
+const ReactGallery = () => {
+    
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}>here showing gallery</h1>
+            <hr />
+
+            
+        </div>
+    );
+};
+
+export default ReactGallery;
---
+++ src/FakeData.js
@@ -0,0 +1,130 @@
+const FakeData=[
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hdd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hddd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hdd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hdd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hdddd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hdddd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+    {
+        "id":1,
+        "image": "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg",
+        "name":"lorem",
+        "catagory":"hd",
+        "price":12,
+        "description":"lorem ipsum dolor sit amet, consectetur adipiscing"
+
+    },
+]
+
+export default FakeData;
